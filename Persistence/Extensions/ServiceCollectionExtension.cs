dc54e5a0a4e5dbfe9381a3792070ed937363fb04
Aliaksandr-Baradzin-Innowise <aliaksandr.baradzin@innowise.com>
2026-02-05 09:23:37 +0100

    Build fix.

--- Persistence/Extensions/ServiceCollectionExtension.cs
+++ Persistence/Extensions/ServiceCollectionExtension.cs
@@ -56,9 +56,4 @@ public static class ServiceCollectionExtension
     {
         services.AddScoped<IDatabaseTransactionManager, DatabaseTransactionManager>();
     }
-
-    public static void AddDatabaseManagers(this IServiceCollection services)
-    {
-        services.AddScoped<IDatabaseTransactionManager, DatabaseTransactionManager>();
-    }
 }
