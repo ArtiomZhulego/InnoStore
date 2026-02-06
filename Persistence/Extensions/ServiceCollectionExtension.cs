f494ba9727b4f4cb367d10cfc29dc6d4f659c6ae
Aliaksandr-Baradzin-Innowise <aliaksandr.baradzin@innowise.com>
2026-02-06 08:42:11 +0100

    Revert  bad changes in "Method fix."

--- Persistence/Extensions/ServiceCollectionExtension.cs
+++ Persistence/Extensions/ServiceCollectionExtension.cs
@@ -19,21 +19,17 @@ public static class ServiceCollectionExtension
     {
         public IServiceCollection AddPersistenceServices(IConfiguration configuration)
         {
-            //var builder = new NpgsqlConnectionStringBuilder
-            //{
-            //    Host = configuration["DB_HOST"],
-            //    Port = int.Parse(configuration["DB_PORT"] ?? "5432"),
-            //    Database = configuration["DB_NAME"],
-            //    Username = configuration["DB_USER"],
-            //    Password = configuration["DB_PASSWORD"]
-            //};
-
-            //services.AddDbContext<InnoStoreContext>(options =>
-            //    options.UseNpgsql(builder.ConnectionString));
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = configuration["DB_HOST"],
+                Port = int.Parse(configuration["DB_PORT"] ?? "5432"),
+                Database = configuration["DB_NAME"],
+                Username = configuration["DB_USER"],
+                Password = configuration["DB_PASSWORD"]
+            };
 
             services.AddDbContext<InnoStoreContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
+                options.UseNpgsql(builder.ConnectionString));
 
             return services;
         }
